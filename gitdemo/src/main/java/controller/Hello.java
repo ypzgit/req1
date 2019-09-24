ca926496c347fc3c241df093cb64a16f751c0df8
ypz <1374262516@qq.com>
2019-09-24 14:29:43 +0800

    zss

---
+++ gitdemo/src/main/java/controller/Hello.java
@@ -0,0 +1,9 @@
+package controller;
+
+public class Hello {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("hello zss");
+        }
+    }
+}
