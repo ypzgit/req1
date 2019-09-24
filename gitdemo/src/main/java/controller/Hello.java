2c259dee868e32479dc343a1885c5abc3784df0d
ypzgit <52305567+ypzgit@users.noreply.github.com>
2019-09-24 15:12:09 +0800

    Update Hello.java

--- gitdemo/src/main/java/controller/Hello.java
+++ gitdemo/src/main/java/controller/Hello.java
@@ -3,7 +3,7 @@ package controller;
 public class Hello {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
-            System.out.println("hello zss");
+            System.out.println("hello ypz");
         }
     }
 }
