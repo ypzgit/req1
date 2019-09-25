2ad8588ecd3d8f18fd6385a17c451d49fd8ae633
ypz <1374262516@qq.com>
2019-09-25 10:05:49 +0800

    ypz修改名字

--- gitdemo/src/main/java/controller/Hello.java
+++ gitdemo/src/main/java/controller/Hello.java
@@ -3,7 +3,7 @@ package controller;
 public class Hello {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
-            System.out.println("hello ypz");
+            System.out.println("hello zss");
         }
     }
 }
